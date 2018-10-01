73b78137adbe773c908b8902e399b9bfc7ad8648
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2018-10-01 13:24:38 -0700

    branch for re-work; TodoList class now combines all methods and fs manipulation while detaching the cli reader and default template; assumption: check/uncheck task command (c)heck can be changed to (t)oggle and further explained in showHelp

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const readline = require('readline');
-const chalk = require('chalk');
-const fs = require('fs');
 
-const success = chalk.green;
-const waiting = chalk.blue;
-
+// cmd line 'console' independent of todoList functionality
+const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
+// default todos.json; examples of todo model
 const template = `[
   {
     "isChecked": false,
@@ -19,131 +16,10 @@ const template = `[
     "isChecked": true,
     "text": "You can remove this template todo!"
   }
-]`;
+]`
 
-function showTodos() {
-  todos.forEach((todo, index)=> {
-    const color = todo.isChecked ? success : waiting;
-    console.log(color(`${index} - [${todo.isChecked ? 'X' : ' ' }] ${todo.text}`))
-  });
-}
+// construct new file/list (with template fall-back) and tie-in 'console'
+const todoList = require('./todo-list')(rl, template)
 
-function askForATask(withHelp) {
-  console.clear();
-
-  if(withHelp) {
-    showHelp();
-  } else {
-    showTodos();
-    console.log('type an option: (a)dd, (c)heck, (r)emove, (h)elp, (e)xit ');
-  }
-  rl.question('> ', (answer) => {
-    [answer, ...args] = answer.split(' ');
-    checkTask(answer, args);
-  });
-}
-function addTodo(text) {
-  if( text.length > 0){
-    todos.push({
-      isChecked: false,
-      text,
-    });
-  }
-}
-function checkTodos(ids){
-  ids.forEach((id) => {
-    if (todos[id]){
-      todos[id].isChecked = !todos[id].isChecked;
-    }
-  });
-}
-function removeTodos(ids){
-  ids.sort((a, b) => b - a);
-  ids.forEach((id) => {
-    if (todos[id]) {
-      todos.splice(id, 1);
-    }
-  });
-}
-function showHelp() {
-  console.log(`
-  ${chalk.bgGreen('TODO LIST NODE CLI')}\n
-  Manage your todos anytime using command line!\n
-  Every change will be saved in your system.\n
-  usage: 'command [arguments]' - the arguments are space separated!\n
-
-  ${chalk.green('add')} - add a new todo. Example ${chalk.inverse('add my new task')}\n
-  ${chalk.blue('check')} - checkmark the  items. Example: ${chalk.inverse('check 0 2')}. this will check the first item and the third.\n
-  ${chalk.red('remove')} - remove items from the list. Example ${chalk.inverse('remove 0 1')}. this will remove the first two items.\n
-  you can use the initial letter of each command for a shortcut\n
-  > PRESS ENTER TO CONTINUE < \n
-`)
-}
-function checkTask(answer, args) {
-  let help = false;
-  switch(answer){
-    case 'a':
-    case 'add':
-      addTodo(args.join(' '));
-      break;
-    case 'c':
-    case 'check':
-      checkTodos(args);
-      break;
-    case 'r':
-    case 'remove':
-      removeTodos(args);
-      break;
-    case 'h':
-    case 'help':
-      help = true;
-      break;
-    case 'e':
-    case 'exit':
-      console.clear();
-      rl.close();
-      process.exit();
-      break;
-    default:
-      help = false;
-
-  }
-  saveData();
-  askForATask(help);
-}
-
-function loadFile() {
-  try{
-    todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
-    askForATask(false);
-  } catch (err){
-    if (err.code = 'ENOENT'){
-      console.log('Todo file not found. do you want generate a new one? (Y/n)');
-      rl.question('> ', (answer) => {
-        switch(answer) {
-            case 'y':
-            case 'Y':
-            case 'YES':
-            case 'yes':
-              fs.writeFileSync('todos.json', template, 'utf8');
-              todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'));
-              askForATask(false);
-              break;
-            default:
-              console.log('Exiting...');
-              process.exit(0);
-        }
-       });
-
-    } else {
-      console.log(err);
-      process.exit(0);
-    }
-  }
-}
-function saveData() {
-  fs.writeFileSync('todos.json', JSON.stringify(todos), 'utf8');
-}
-let todos;
-loadFile();
+todoList.load()
 
---
+++ todo-list.js
@@ -0,0 +1,142 @@
+const fs = require('fs')
+
+const chalk = require('chalk')
+const success = chalk.green
+const waiting = chalk.blue
+
+const taskMap = {
+  add: {
+    function: null,
+    cases: [ 'a', 'add' ],
+  },
+  toggle: {
+    function: null,
+    cases: [ 't', 'toggle' ],
+  },
+  remove: {
+    function: null,
+    cases: [ 'r', 'remove' ],
+  },
+  help: {
+    function: null,
+    cases: [ 'h', 'help' ],
+  },
+  exit: {
+    function: null,
+    cases: [ 'e', 'exit' ],
+  },
+}
+
+const displayTodos = (todo, index)=> {
+  const display = todo.isChecked ? success : waiting
+  console.log(display(`${index} - [${todo.isChecked ? 'X' : ' ' }] ${todo.text}`))
+}
+
+class TodoList {
+  constructor(rl, template) {
+    this.todos = JSON.parse(template)
+    this.cli = rl
+  }
+  saveData() {
+    fs.writeFileSync('todos.json', JSON.stringify(this.todos), 'utf8')
+  }
+  load() {
+    const newFilePrompt = answer => {
+      switch(answer) {
+          case 'y':
+          case 'Y':
+          case 'YES':
+          case 'yes':
+            this.saveData()
+            this.askForATask(false)
+            break
+          default:
+            console.log('Exiting...')
+            process.exit(0)
+      }
+    }
+    try{
+      this.todos = JSON.parse(fs.readFileSync('todos.json', 'utf8'))
+      this.askForATask(false)
+    } catch (err){
+      if (err.code = 'ENOENT'){
+        console.log('Todo file not found. do you want generate a new one? (Y/n)')
+        this.cli.question('> ', newFilePrompt)
+      } else {
+        console.log(err)
+        process.exit(0)
+      }
+    }
+  }
+  showTodos() {
+    this.todos.forEach(displayTodos)
+  }
+
+  addTodo(text) {
+    if(text.length > 0){
+      this.todos.push({ isChecked: false,  text })
+    }
+  }
+  toggleTodos(ids) {
+    ids.forEach(id => {
+      if (this.todos[id]) {
+        this.todos[id].isChecked = !this.todos[id].isChecked
+      }
+    })
+  }
+  removeTodos(ids) {
+    this.todos = this.todos.filter((_, id) => !ids.includes(`${id}`))
+  }
+
+  runTask(answer, args) {
+    if (taskMap.help.cases.includes(answer)) {
+      this.showHelp()
+      return
+    }
+    if (taskMap.add.cases.includes(answer)) {
+      this.addTodo(args.join(' '))
+    } else if (taskMap.toggle.cases.includes(answer)) {
+      this.toggleTodos(args)
+    } else if (taskMap.remove.cases.includes(answer)) {
+      this.removeTodos(args)
+    } else if (taskMap.exit.cases.includes(answer)) {
+      console.clear()
+      this.cli.close()
+      process.exit()
+    }
+    this.saveData()
+    this.askForATask()
+  }
+  askForATask() {
+    console.clear()
+    this.showTodos()
+
+    console.log('type an option: (a)dd, (t)oggle, (r)emove, (h)elp, (e)xit ')
+    this.cli.question('> ', answer => {
+      const [ans, ...args] = answer.trim().split(' ')
+      this.runTask(ans, args)
+    })
+  }
+  showHelp() {
+    console.clear()
+
+    console.log(`
+      ${chalk.bgGreen('TODO LIST NODE CLI')}\n
+      Manage your todos anytime using command line!\n
+      Every change will be saved in your system.\n
+      usage: 'command [arguments]' - the arguments are space separated!\n
+
+      ${chalk.green('add')} - add a new todo. \n
+        Example ${chalk.inverse('add my new task')}\n
+      ${chalk.blue('toggle')} - toggle the checkmark of the item(s). \n
+        Example: ${chalk.inverse('toggle 0 2')}. this will check/uncheck the first item and the third.\n
+      ${chalk.red('remove')} - remove items from the list. \n
+        Example ${chalk.inverse('remove 0 1')}. this will remove the first two items.\n
+      you can use the initial letter of each command for a shortcut\n
+      > PRESS ENTER TO CONTINUE < \n
+    `)
+    this.cli.question('> ', answer => this.askForATask() )
+  }
+}
+
+module.exports = (rl, template) => new TodoList(rl, template)
